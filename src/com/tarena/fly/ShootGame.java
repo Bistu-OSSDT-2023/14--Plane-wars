9cea137f2dbbb03a841679cd68d00658ac832e58
moyanislth <81506667+moyanislth@users.noreply.github.com>
2023-07-05 10:33:59 +0800

    4
    加速生成，子弹减速

--- src/com/tarena/fly/ShootGame.java
+++ src/com/tarena/fly/ShootGame.java
@@ -206,7 +206,7 @@ public class ShootGame extends JPanel {
 	/** 飞行物入场 */
 	public void enterAction() {
 		flyEnteredIndex++;
-		if (flyEnteredIndex % 40 == 0) { // 400毫秒生成一个飞行物--10*40
+		if (flyEnteredIndex % 30 == 0) { // 300毫秒生成一个飞行物--10*40
 			FlyingObject obj = nextOne(); // 随机生成一个飞行物
 			flyings = Arrays.copyOf(flyings, flyings.length + 1);
 			flyings[flyings.length - 1] = obj;
@@ -240,7 +240,7 @@ public class ShootGame extends JPanel {
 	/** 射击 */
 	public void shootAction() {
 		shootIndex++;
-		if (shootIndex % 30 == 0) { // 300毫秒发一颗
+		if (shootIndex % 90 == 0) { //900毫秒发一颗
 			Bullet[] bs = hero.shoot(); // 英雄打出子弹
 			bullets = Arrays.copyOf(bullets, bullets.length + bs.length); // 扩容
 			System.arraycopy(bs, 0, bullets, bullets.length - bs.length,
@@ -309,9 +309,11 @@ public class ShootGame extends JPanel {
 		return hero.getLife() <= 0;
 	}
 
+	
 	/** 子弹和飞行物之间的碰撞检查 */
 	public void bang(Bullet bullet) {
 		int index = -1; // 击中的飞行物索引
+		// 遍历所有飞行物
 		for (int i = 0; i < flyings.length; i++) {
 			FlyingObject obj = flyings[i];
 			if (obj.shootBy(bullet)) { // 判断是否击中
@@ -319,14 +321,21 @@ public class ShootGame extends JPanel {
 				break;
 			}
 		}
-		if (index != -1) { // 有击中的飞行物
+		// 有击中的飞行物
+		if (index != -1) { 
 			FlyingObject one = flyings[index]; // 记录被击中的飞行物
+			
+
 
 			FlyingObject temp = flyings[index]; // 被击中的飞行物与最后一个飞行物交换
 			flyings[index] = flyings[flyings.length - 1];
 			flyings[flyings.length - 1] = temp;
 
 			flyings = Arrays.copyOf(flyings, flyings.length - 1); // 删除最后一个飞行物(即被击中的)
+
+			
+
+
 
 			// 检查one的类型(敌人加分，奖励获取)
 			if (one instanceof Enemy) { // 检查类型，是敌人，则加分
@@ -336,16 +345,17 @@ public class ShootGame extends JPanel {
 				Award a = (Award) one;
 				int type = a.getType(); // 获取奖励类型
 				switch (type) {
-				case Award.DOUBLE_FIRE:
-					hero.addDoubleFire(); // 设置双倍火力
-					break;
-				case Award.LIFE:
-					hero.addLife(); // 设置加命
-					break;
+					case Award.DOUBLE_FIRE:
+						hero.addDoubleFire(); // 设置双倍火力
+						break;
+					case Award.LIFE:
+						hero.addLife(); // 设置加命
+						break;
 				}
 			}
 		}
 	}
+
 
 	/**
 	 * 随机生成飞行物
